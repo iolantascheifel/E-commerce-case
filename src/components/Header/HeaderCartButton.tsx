d713ac831455fbef9b7757a1b6f9857a4018a992
iolantascheifel <iolanta.scheifel@gmail.com>
2023-02-09 10:24:53 +0100

    initial design of the cart button, will be changed later

--- src/components/Header/HeaderCartButton.tsx
+++ src/components/Header/HeaderCartButton.tsx
@@ -5,7 +5,7 @@ const HeaderCartButton = () => {
   return (
     <CartButton>
       <CartIcon />
-      <span>{}</span>
+      <Badge>3</Badge>
     </CartButton>
   );
 };
@@ -16,4 +16,18 @@ const CartButton = styled.button`
   border: none;
   background-color: transparent;
   cursor: pointer;
+  position: relative;
+  padding: 15px 15px;
+`;
+
+const Badge = styled.span`
+  background-color: #ee9b0c;
+  opacity: 80%;
+  padding: 5px 10px;
+  border-radius: 25px;
+  margin-left: 1rem;
+  font-weight: bold;
+  position: absolute;
+  bottom: 10px;
+  right: 0;
 `;
